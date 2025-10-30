d8ebc8487887704801b66ea794d299a1fe68e0dd
Toma Sinanski <tsinanski@cps.edu>
2025-10-30 11:28:57 -0500

    did asisngment

--- notes.py
+++ notes.py
@@ -1,4 +1,12 @@
 # object oriented programming
 
 # (define-struct dog [fur_color name age favorite_food])
+class Dog:
+    def __init__(self, breed, fur_color, name, age):
+        self.breed = breed
+        self.fur_color = fur_color
+        self.name = name
+        self.age = age
 
+if __name__ == "__main__":
+    berg_dog = Dog("labrador", "black", "Logan", 9)
